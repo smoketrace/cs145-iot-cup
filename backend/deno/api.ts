72092056d020740d26dbe931d48dd15bfb59b449
eyronjuude <actanael@up.edu.ph>
2023-05-24 20:47:20 +0800

    feat: add `deviceInfo` object for additional device information

--- backend/deno/api.ts
+++ backend/deno/api.ts
@@ -3,12 +3,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebas
 import { collection, getFirestore, addDoc, doc, query, where, getDocs } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
 import { oakCors } from "https://deno.land/x/cors/mod.ts";
 
-
 const firebaseConfig = JSON.parse(Deno.env.get("FIREBASE_CONFIG"));
-
-// Dictionary to store device timers
-const devices = new Map<string, ReturnType<typeof setTimeout>>(); // Create a device map that stores the timeout handlers for each device
-
 const firebaseApp = initializeApp(firebaseConfig, "smoketrace-145");
 const db = getFirestore(firebaseApp);
 
@@ -17,6 +12,22 @@ type sensorData = {
     smoke_read: number;
     time: string;
 };
+
+// Define status constants for deviceInfo type
+const GREEN = 0;
+const ORANGE = 1;
+const RED = 2;
+
+// Define deviceInfo type for containing device status and information
+type deviceInfo = {
+    status: number; // Perceived status of the device
+    last_read: number; // Last smoke_read of the device
+    last_alive: string; // Last time the device sent a POST message to the server
+    timeout_handler: ReturnType<typeof setTimeout>; // Timeout handler for the device
+};
+
+// Dictionary to store device timers
+const devices = new Map<string, deviceInfo>(); // Create a device map that stores the timeout handlers for each device
 
 // Create instance of App Server
 const app = new Application();
@@ -75,14 +86,27 @@ router
         
         // Refresh timer for device_id if the device already exists in the device map
         if (devices.has(device_id)){ // If the device ID already exists in the device map
-            const device_timer = devices.get(device_id); // Obtain the handler identifier stored in the device map
-            clearTimeout(device_timer); // Deactivate timer set with handler identifier stored in the device map
+            const device_timer: ReturnType<typeof setTimeout> = devices.get(device_id).timeout_handler; // Obtain the timeout handler identifier stored in the device map
+            clearTimeout(device_timer); // Deactivate timer set with timeout handler identifier stored in the device map
         }
 
-        // Allocate new asynchronous timer to the device
-        devices.set(device_id, setTimeout(() => {
-            console.log(`${device_id} did not respond for 15 seconds`); // Print to console upon 15 seconds of not POST-ing
-        }, 15000)); // Set timeout for 15 seconds
+        // Create timeout handler for the logged device
+        const timeout_handler = setTimeout(() => {
+            devices.get(device_id).status = ORANGE; // If the timer expires, set the device status to ORANGE
+            console.log(`${device_id} did not respond for 15 seconds`); // Print to console upon 15 seconds of not POST-ing (debug)
+            console.log(devices.get(device_id)); // Print to console about the latest device information of the unresponsive device
+        }, 15000); // Set timeout for 15 seconds
+
+        // Create mutable deviceInfo entry of device_id in the device map
+        const device_info: deviceInfo = {
+            GREEN, // Set status to GREEN upon receiving the POST request
+            smoke_read, // Set last_read to received smoke_read
+            time, // Set last_alive to received time
+            timeout_handler, // Store the timeout handler for the current device
+        };
+
+        // Update device information of the device in the device map
+        devices.set(device_id, device_info); // Set timeout for 15 seconds
 
         // Case handling if device id or time is blank - maybe change to case if received info is incorrect?
         if (!device_id || !time) {
