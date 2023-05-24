754c6e9b679a52276faee5d7b9bed4cf9100581e
Clawber <jssarenas@up.edu.ph>
2023-05-25 04:18:00 +0800

    build: added fetching data from api

--- frontend/src/routes/chart/+page.svelte
+++ frontend/src/routes/chart/+page.svelte
@@ -3,6 +3,27 @@
 	<meta name="description" content="View smoke readings on a graph" />
 </svelte:head>
 
+
+
+
+
+<script lang="ts">
+  import { onMount } from 'svelte'
+
+  let posts: any = {}
+
+
+	let api_url = "https://smoketrace-api.deno.dev/sensors/ESP32_JOHN"
+  onMount(() => {
+    fetch(api_url)
+      .then(response => response.json())
+      .then(result => {
+				console.log(result);
+				posts = result
+			})
+  })
+</script>
+
 <div>
 	<h1>Smoke Chart</h1>
 
