3aa7ae2fb5f359b660902af9c6a618474b2c2d4e
Clawber <jssarenas@up.edu.ph>
2023-05-26 21:44:58 +0800

    fix: fixed promise handling to fix null value bug

--- frontend/src/routes/chart/+page.svelte
+++ frontend/src/routes/chart/+page.svelte
@@ -11,21 +11,37 @@
   import { onMount } from 'svelte';
   import { fetchFromAPI } from '$lib/helpers/fetch'
 
+
   Chart.register(...registerables);
  
-  let barChartElement: HTMLCanvasElement;
   let lineGraph: HTMLCanvasElement;
 
   let sensor_data: any = {}
 
   onMount(() => {
-    // fetching from api
-    sensor_data = fetchFromAPI()
+    function parseSensorValue() {
+      
+    }
 
-  new Chart(lineGraph, {
-    type: 'line', //this denotes tha type of chart
+    // // fetching from api
+    fetchFromAPI()
+      .then(data => {
+        console.log('Data', data);
+        // parsejson here
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      })
 
-    data: {// values on X-Axis
+    function parseSensorData() {
+
+    }
+    
+    
+
+    // parsing json data to be used as graph
+    //  what if only 1 data set (only 1 color)
+    let graphData = {
       labels: ['2022-05-10', '2022-05-11', '2022-05-12','2022-05-13',
               '2022-05-14', '2022-05-15', '2022-05-16','2022-05-17', ], 
       datasets: [
@@ -42,12 +58,17 @@
           backgroundColor: 'limegreen'
         }  
       ]
-    },
+    }
+
+
+  // creating the graph
+  new Chart(lineGraph, {
+    type: 'line', //this denotes tha type of chart
+    data: graphData,
     options: {
       aspectRatio:2.5
     }
-  }
-  )
+  })
 
       
 })
@@ -60,8 +81,7 @@
 	<h1>Smoke Chart</h1>
 
 	<p>
-		[View smoke readings on a graph]
+		[Add button to refresh graph]
 	</p>
-  <canvas bind:this={barChartElement} />
   <canvas bind:this={lineGraph} />
 </div>
