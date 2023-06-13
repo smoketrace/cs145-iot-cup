a884b7cbfbca518d3f025fbd0295eaf98965c1d1
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2023-06-14 01:25:58 +0800

    feat(frontend): display custom message for status

--- frontend/src/lib/components/HealthLogItem.svelte
+++ frontend/src/lib/components/HealthLogItem.svelte
@@ -1,26 +1,31 @@
 <script lang="ts">
-    enum STATUS {
-        GREEN,
-        ORANGE,
-        RED,
-        BLACK,
-        RECON,
-        SMS,
-    }
-
-    export let status: STATUS;
+    import { STATUS } from '../../lib/helpers/types';
+    export let status: number;
     export let seconds: number;
     export let device_id: string;
 
     let timestamp = new Date(seconds*1000).toLocaleString();
+
+    const statusMessages = [
+        "is healthy.",
+        "has disconnected.",
+        "is detecting high smoke levels.",
+        "has disconnected while reading high smoke levels.",
+        "has reconnected.",
+        "sent an SMS alert to ",
+        "reading consistently high smoke levels. Please contact the fire department.",
+    ]
+
+    let status_color: string = STATUS[status];
 
 </script>
 
 <li>
     <span class="dot"></span>
     <p>{timestamp}</p>
-    <h3>Sensor status report: {status}</h3>
-    <p>{device_id} has status</p>
+    <h3>{status_color} Status</h3>
+    <span>{device_id}</span>
+    <span>{statusMessages[status]}</span>
 </li>
 
 <style>
